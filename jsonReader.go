3a196c438206b133ecababd68d52e81144962a20
Louis Pocheron <louis.pocheron@davikingcode.com>
2025-11-11 21:51:16 +0100

    wip

--- jsonReader.go
+++ jsonReader.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"time"
 )
 
 type JsonReader interface {
 	openFile() *os.File
 	GetAll() []Todo
-	get(id uint8)
+	Get(id uint8) Todo
 	update(id uint8)
 	remove(id uint)
 	Create() Todo
@@ -29,30 +30,41 @@ func (reader Reader) openFile() *os.File {
 	return file
 }
 
-func (reader Reader) GetAll() []Todo {
+func (reader Reader) GetAll() ([]Todo, error) {
 	file := reader.openFile()
 	defer file.Close()
 	var todos []Todo
 	err := json.NewDecoder(file).Decode(&todos)
 
 	if err != nil && err.Error() != "EOF" {
-		log.Fatal("couldnt decode the file", err);
+		return nil, err
 	}
 
-	println(todos)
-	return todos
+	return todos, nil
 }
 
 func (reader Reader) Create(todo Todo) {
 	file := reader.openFile()
 	defer file.Close()
 
-	todos := reader.GetAll()
+	todos, err := reader.GetAll()
+
+	if err != nil {
+		log.Fatal("could not get all todos: ", err)
+	}
+
+	var id uint8
+	if len(todos) == 0 {
+		id = 1
+	} else {
+		id = todos[len(todos)-1].Id + 1
+	}
+
+	todo.Id = id
 
 	todos = append(todos, todo)
-	
-	_, err := file.Seek(0, 0)
-	println("bruhg")
+
+	_, err = file.Seek(0, 0)
 
 	if err != nil && err.Error() != "EOF" {
 		log.Fatal("ici", err)
@@ -68,6 +80,40 @@ func (reader Reader) Create(todo Todo) {
 
 	if err != nil {
 		log.Fatal("could not write to file: ", err)
+	}
+}
+
+func (reader Reader) Get(id uint8) (Todo, bool) {
+	file := reader.openFile()
+	defer file.Close()
+	todos, err := reader.GetAll()
+
+	if err != nil {
+		log.Fatal("could not get all todos: ", err)
+	}
+
+	for _, todo := range todos {
+		if todo.Id == id {
+			return todo, true
+		}
+	}
+	return Todo{}, false
+}
+
+func (reader Reader) update(id uint8) {
+	file := reader.openFile()
+	defer file.Close()
+
+	var todos []Todo
+	err := json.NewDecoder(file).Decode(&todos)
+	if err != nil && err.Error() != "EOF" {
+		log.Fatal("couldnt decode the file", err)
+	}
+
+	for i, todo := range todos {
+		if todo.Id == id {
+			todos[i].UpdatedAt = time.Now()
+		}
 	}
 }
 
--- server.go
+++ server.go
@@ -5,15 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
-const PORT  = ":8080"
+const PORT = ":8080"
 
 type Todo struct {
-	Id uint8 `json:"id"`
-	Title string `json:"name"`
-	Content string `json:"content"`
+	Id        uint8     `json:"id"`
+	Title     string    `json:"name"`
+	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
@@ -23,7 +24,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("reached 8080/")
 
-		file, err := os.Open("test.json");
+		file, err := os.Open("test.json")
 		if err != nil {
 			log.Println("could not open test.json")
 			file.Close()
@@ -31,28 +32,63 @@ func main() {
 		}
 		defer file.Close()
 
-		fmt.Fprintln(w, "yo bro");
+		fmt.Fprintln(w, "yo bro")
 	})
 
-
 	http.HandleFunc("/create", func(writer http.ResponseWriter, r *http.Request) {
-
-		todo := Todo {
-			Id: 0,
-			Title: "my first todo",
-			Content: "un contenu comme un autre",
+		todo := Todo{
+			Title:     "my first todo",
+			Content:   "un contenu comme un autre",
 			CreatedAt: time.Now(),
 			UpdatedAt: time.Now(),
 		}
-
 		reader.Create(todo)
-
-		fmt.Fprintln(writer, "Todo created");
-	
+		fmt.Fprintln(writer, "Todo created")
 	})
 
+	http.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		param := r.PathValue("id")
 
+		if param == "" {
+			log.Println("id parameter is missing")
+			http.Error(w, "id parameter is missing", http.StatusBadRequest)
+			return
+		}
 
-	fmt.Println("starting server listening on port",PORT)
+		println("param:", param)
+		id, err := strconv.Atoi(param)
+
+		if err != nil {
+			log.Println("invalid id parameter", err)
+			log.Fatal("invalid id parameter", err)
+			http.Error(w, "invalid id parameter", http.StatusBadRequest)
+			return
+		}
+
+		todo, success := reader.Get(uint8(id))
+
+		if !success {
+			log.Println("todo not found")
+			http.Error(w, "todo not found", http.StatusNotFound)
+			return
+		}
+
+		log.Println("reached 8080/{id}")
+		fmt.Fprintln(w, "Todo:", todo)
+	})
+
+	http.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request) {
+		todos, err := reader.GetAll()
+		if err != nil {
+			log.Println("could not get all todos:", err)
+			http.Error(w, "could not get all todos", http.StatusInternalServerError)
+			return 
+		}
+
+		log.Println("reached 8080/all")
+		fmt.Fprintln(w, "Todos:", todos)
+	})
+
+	fmt.Println("starting server listening on port", PORT)
 	http.ListenAndServe(PORT, nil)
 }
